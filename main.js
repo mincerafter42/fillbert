83a33a08cbbe74d49c4c6d8ab01a6df425edcbaa
mincerafter42 <mincerafter42@gmail.com>
2021-06-27 20:16:40 -0700

    Made website have light text on a dark background (except for the comics themselves.) Changes to dark text on a light background when printing. Removed Internet Explorer support in main.js because it's easier to not support the browser almost nobody uses.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 function copy(text) {
-	if (window.clipboardData) window.clipboardData.setData("Text", text); // IE
-	else navigator.clipboard.writeText(text);
+	navigator.clipboard.writeText(text);
 }
