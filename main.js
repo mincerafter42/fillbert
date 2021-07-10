864a6e58e8d6024ae60ac687ac029fac54d70e37
mincerafter42 <mincerafter42@gmail.com>
2021-07-10 08:31:32 -0700

    Added animations checkbox to header navigation. Uses JavaScript to disable animations if window.localStorage.disableAnimations==true Not shown if user has JavaScript disabled.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function copy(text) {
 	navigator.clipboard.writeText(text);
 }
+
+function removeElementsWithTagName(tagName) {
+	while (document.getElementsByTagName(tagName).length > 0) document.getElementsByTagName(tagName)[0].remove()
+}
+
+function localStorageAndReload(key, value) {
+	try {
+		window.localStorage.setItem(key, value);
+		location.reload();
+	} catch(e) {alert(e.message)}
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+    if (window.localStorage.getItem("disableAnimations")=="true") {
+    	removeElementsWithTagName("animate");
+    	removeElementsWithTagName("animateTransform");
+    	removeElementsWithTagName("animateMotion");
+    	document.getElementById("disableAnimations").removeAttribute("checked");
+    }
+});
